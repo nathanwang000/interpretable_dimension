b5e80e43cd9bdee087a9dbfdfbc2f57f1f1e3dd6
Jiaxuan Wang <nathanwang000@gmail.com>
2018-08-14 12:25:46 -0400

    before refactor commit

--- src/utility.py
+++ src/utility.py
@@ -24,7 +24,7 @@ def preprocess_image(cv2im, resize_im=True):
     if resize_im:
         cv2im = cv2.resize(cv2im, (224, 224))
     im_as_arr = np.float32(cv2im)
-    im_as_arr = np.ascontiguousarray(im_as_arr[..., ::-1])
+    im_as_arr = np.ascontiguousarray(im_as_arr[..., ::-1]) # from bgr to rgb
     im_as_arr = im_as_arr.transpose(2, 0, 1)  # Convert array to D,W,H
     # Normalize the channels
     for channel, _ in enumerate(im_as_arr):
